c50ee1bde9bda622b6a612c2d042a3a8ec61e183
mattsGitHub123 <mattjoy99@gmail.com>
2016-04-30 17:59:05 -0400

    updated

--- One_Click_Code.ino
+++ One_Click_Code.ino
@@ -1,5 +1,6 @@
 #include <RBL_nRF8001.h>
 #include <RBL_services.h>
+#include <math.h>
 
 
 /**
@@ -18,8 +19,8 @@
 #define BLE_NAME     "BLE SHEILD"
 
 int photocellData = 0;         // value read from the sensor
-float thermisterData = 0;      // Value to be read from the sensor
 int serviceStarted = 0;
+char thermisterArray[6];
 
 void setup()
 {
@@ -38,21 +39,23 @@ void startBLEService()
 
 void readFromPhotoCell()
 {
-  Serial.print("In read from photo cell");
+  Serial.println("In read from photo cell");
   photocellData = analogRead(photocellPin); //read the value of A0
   Serial.print("Photo Resister data: ");
-  Serial.print(photocellData);
+  Serial.println(photocellData);
+ 
 }
 
 void readFromThermister()
 {
-  Serial.print("in read from thermister");
+  Serial.println("in read from thermister");
   long thermisterData = analogRead(analogPin);
   
   //the calculating formula of temperature
   float tempC = beta /(log((1025.0 * 10 / thermisterData - 10) / 10) + beta / 298.0) - 273.0;
   float tempF = 1.8 * tempC + 32.0;
-  thermisterData = tempF;
+
+  dtostrf(tempF, 4, 2, thermisterArray);
   
   //Serial.print("TempC: ");//print" TempC: "
   //Serial.print(tempC);//print Celsius temperature
@@ -60,6 +63,8 @@ void readFromThermister()
   //Serial.println();
   Serial.print(" TempF: ");
   Serial.print(tempF);
+  Serial.print(" ");
+  Serial.print(thermisterArray);
   //Serial.print(" F");
   Serial.println();
 }
@@ -74,7 +79,6 @@ void loop()
       startBLEService();
       
   }
-
  
   if (ble_connected() && serviceStarted)
   {
@@ -91,16 +95,37 @@ void loop()
 
         Serial.println("Connected to Iphone and received 1");
         Serial.println("Dumping Data....");
+
+        Serial.print("thermisterArray ");
+        Serial.print(thermisterArray);
         
-        ble_write(photocellData);
-        ble_write(thermisterData);
-        ble_write(0);
-        serviceStarted = 0;
+        ble_write(thermisterArray[0]);
+        ble_write(thermisterArray[1]);
+        ble_write(thermisterArray[2]);
+        ble_write(thermisterArray[3]);
+        ble_write(thermisterArray[4]);
+
+        ble_do_events();
+        
+        int temp = (photocellData >> 8 ) & 0xff;
+        Serial.print("Low bit: ");
+        Serial.print(temp);
+        ble_write(temp);
+        temp = (photocellData) & 0xff;
+        Serial.print(" High byte: ");
+        Serial.print(temp);
+        ble_write(temp);
+       
+        
+      
+        
+        
+       // serviceStarted = 0;
      
       }
     }
   }
   ble_do_events();
-  //delay(2000); //wait for 2000 milliseconds
+  
 
 }
